b04d6d3a2976182e54e803bc9808a44ca29ad188
liul91512 <875150561@qq.com>
2020-05-28 18:18:24 +0800

    update 123

---
+++ test_02.py
@@ -0,0 +1,11 @@
+import pytest
+
+class TestCase:
+    def test_004(self):
+        print("这是测试用例4")
+        
+    def test_005(self):
+        print("这是测试用例5")
+
+    def test_006(self):
+        print("这是测试用例6")
