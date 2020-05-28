c7c38038cd476fb8a133633454992a4d8599ed94
liul91512 <875150561@qq.com>
2020-05-28 17:53:27 +0800

    auto test

---
+++ 123.py
@@ -0,0 +1,11 @@
+import pytest
+
+class TestCase:
+    def test_001(self):
+        print("这是测试用例1")
+        
+    def test_002(self):
+        print("这是测试用例2")
+
+    def test_003(self):
+        print("这是测试用例3")
